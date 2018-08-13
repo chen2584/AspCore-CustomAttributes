b438da73e462823f7c6e9b40fda17b96977eda6c
Worameth Semapat <31945018+chen2584@users.noreply.github.com>
2018-08-13 15:56:24 +0700

    Refactor & add JsonResult cache

--- OutputCache.cs
+++ OutputCache.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -29,7 +30,6 @@ namespace chen2584.CustomAttributes
                     Content = outputCache.Content
                 };
             }
-
         }
 
         public override async void OnResultExecuted(ResultExecutedContext context)
@@ -37,36 +37,64 @@ namespace chen2584.CustomAttributes
             string cacheKey = $"{context.RouteData.Values["controller"].ToString()}-{context.RouteData.Values["action"].ToString()}";
             if (cache.Get(cacheKey) == null)
             {
+                OutputCacheModel outputCache = null;
                 if (context.Result is ObjectResult objectResult)
                 {
-                    string value = context.HttpContext.Response.ContentType.StartsWith("application/json") ? JsonConvert.SerializeObject(objectResult.Value, jsonSettings) : objectResult.Value.ToString();
-                    var outputCache = new OutputCacheModel() { Content = value, ContentType = context.HttpContext.Response.ContentType, StatusCode = context.HttpContext.Response.StatusCode };
-                    cache.Set(cacheKey, outputCache, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromSeconds(Duration)));
+                    // Check if objectResult is JSON
+                    string value = context.HttpContext.Response.ContentType.StartsWith("application/json") 
+                        ? JsonConvert.SerializeObject(objectResult.Value, jsonSettings) : objectResult.Value.ToString();
+                    outputCache = new OutputCacheModel()
+                    { 
+                        Content = value,
+                        ContentType = context.HttpContext.Response.ContentType,
+                        StatusCode = context.HttpContext.Response.StatusCode 
+                    };
+                }
+                else if(context.Result is JsonResult jsonResult)
+                {
+                    outputCache = new OutputCacheModel()
+                    { 
+                        Content = JsonConvert.SerializeObject(jsonResult.Value, jsonSettings),
+                        ContentType = context.HttpContext.Response.ContentType,
+                        StatusCode = context.HttpContext.Response.StatusCode 
+                    };
                 }
                 else if (context.Result is ViewResult viewResult)
                 {
+                    outputCache = await GenerateViewAsString(context, viewResult);
+                }
 
-                    string viewName = viewResult.ViewName != null ? viewResult.ViewName : context.RouteData.Values["action"].ToString();
-                    using (var writer = new StringWriter())
+                if(outputCache != null)
+                {
+                    cache.Set(cacheKey, outputCache, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromSeconds(Duration)));
+                }
+            }
+        }
+
+        private async Task<OutputCacheModel> GenerateViewAsString(FilterContext context, ViewResult viewResult)
+        {
+            string viewName = viewResult.ViewName != null ? viewResult.ViewName : context.RouteData.Values["action"].ToString();
+            IViewEngine viewEngine = context.HttpContext.RequestServices.GetService(typeof(ICompositeViewEngine)) as ICompositeViewEngine;
+            ViewEngineResult viewEngineResult = viewEngine.FindView(context, viewName, true);
+            using (var writer = new StringWriter())
+            {
+                if (viewEngineResult.Success)
+                {
+                    ViewContext viewContext = new ViewContext(context, viewEngineResult.View, viewResult.ViewData, viewResult.TempData, writer, new HtmlHelperOptions());
+                    await viewEngineResult.View.RenderAsync(viewContext);
+
+                    string content = writer.GetStringBuilder().ToString();
+                    var outputCache = new OutputCacheModel()
                     {
-                        IViewEngine viewEngine = context.HttpContext.RequestServices.GetService(typeof(ICompositeViewEngine)) as ICompositeViewEngine;
-                        ViewEngineResult viewEngineResult = viewEngine.FindView(context, viewName, true);
-
-                        if (viewEngineResult.Success)
-                        {
-                            ViewContext viewContext = new ViewContext(context, viewEngineResult.View, viewResult.ViewData, viewResult.TempData, writer, new HtmlHelperOptions());
-                            await viewEngineResult.View.RenderAsync(viewContext);
-
-                            string content = writer.GetStringBuilder().ToString();
-                            var outputCache = new OutputCacheModel() { Content = content, ContentType = context.HttpContext.Response.ContentType, StatusCode = context.HttpContext.Response.StatusCode };
-                            cache.Set(cacheKey, outputCache, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromSeconds(Duration)));
-
-                        }
-                        else
-                        {
-                            Console.WriteLine($"A view with the name {viewName} could not be found");
-                        }
-                    }
+                        Content = content,
+                        ContentType = context.HttpContext.Response.ContentType,
+                        StatusCode = context.HttpContext.Response.StatusCode
+                    };
+                    return outputCache;
+                }
+                else
+                {
+                    throw new FileNotFoundException($"A view with the name {viewName} could not be found");
                 }
             }
         }
